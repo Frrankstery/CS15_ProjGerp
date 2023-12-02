7f303ffea29abc7963c2a53e581a96e53815f754
Yunzhe Li <55543424+Frrankstery@users.noreply.github.com>
2023-12-02 18:00:19 -0500

    Update stringProcessing.cpp
    fixed the error I had yesterday

--- stringProcessing.cpp
+++ stringProcessing.cpp
@@ -11,22 +11,21 @@ string stringProcessing::stripNonAlphaNum (std::string input) {
     for (int i = 0; i < len; i++) {
         if (isAlphaNum(input[i])) {
             word << input[i];
-            cout << "word: " << word.str() << endl;
         }
 
     }
-    cout << word.str() << endl;
-    cout << "test" << endl;
     return word.str();
-    
 }
 
 bool stringProcessing::isAlphaNum (char input) {
-    if (int(input) > 90 or int(input) < 65) {
-        return false;
+    cerr  << int(input) << endl;
+    if (int(input) < 90 and int(input) > 65 ) {
+        return true;
     }
-    if (int(input) > 122 or int(input) < 97) {
-        return false;
+    if (int(input) < 122 and int(input) > 97) {
+        return true;
+        
     }
-    return true;
+    cerr << "executing isAlphaNum: " << endl;
+    return false;
 }
