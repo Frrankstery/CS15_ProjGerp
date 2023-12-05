2bd5dab2c613d114e4428f72260a90c27cdb4d67
Yunzhe Li <55543424+Frrankstery@users.noreply.github.com>
2023-12-04 19:44:46 -0500

    Update stringProcessing.h

--- stringProcessing.h
+++ stringProcessing.h
@@ -1,3 +1,12 @@
+/*
+file: stringProcessing.h
+name: Cullen, Frank
+date: December 4, 2023
+project: proj 4 gerp
+description: this is a class that contains the method of stripping non-alpha
+        characters from the input string
+*/
+
 #ifndef STRINGPROCESSING_H
 #define STRINGPROCESSING_H
 
@@ -7,9 +16,24 @@ using namespace std;
 
 class stringProcessing {
 public:
+/*
+stripNonAlphaNum
+purpose: strip all the non-alphanumeric (alphabetic) characters from the given 
+        input.
+Input: string input
+output: string without the non-alphanumeric characters
+*/
 string stripNonAlphaNum (std::string input);
 
 private:
+/*
+isAlphaNum
+purpose: private class function used to determine whether the given char
+        is an alphanumeric character or not
+Input: char input
+output: boolean value that returns true if the char is an alphanumeric and false
+        otherwise
+*/
 bool isAlphaNum (char input);
 
 };
