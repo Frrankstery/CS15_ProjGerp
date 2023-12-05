04f9fbc465a2208b02d9e95ed8832c9129a5d71b
cmccal04 <152649028+cmccal04@users.noreply.github.com>
2023-12-04 19:45:19 -0500

    Update FSTreeTraversal.cpp

--- FSTreeTraversal.cpp
+++ FSTreeTraversal.cpp
@@ -1,3 +1,16 @@
+/*
+ *  FSTreeTraversal.cpp
+ *  Cullen McCaleb and Frank Li
+ *  12/04
+ *
+ *  CS 15 Project 4 Gerp
+ *
+ *  FSTreeTraversal.cpp is a program that prints every directory to file path 
+ *  in a given directory. This is done using an n-ary tree data structure,
+ *  built from the given DirNode and FSTree classes.
+ *
+ */
+
 #include <string>
 #include "FSTree.h"
 #include "DirNode.h"
@@ -6,9 +19,17 @@
 
 using namespace std;
 
+//function declarations
 void traverseTree(std::string directory);
 void traverseTreeHelper(DirNode *node, std::vector<string>& path);
 
+
+/* traverseTree()
+* Purpose: Creates a tree from a given directory, and prints every path in 
+           that tree, from the directory name to the file name
+* Input: A string representing the directory name.
+* Return: Nothing
+*/
 void traverseTree(std::string directory){
     FSTree myTree(directory);
     DirNode *root = myTree.getRoot();
@@ -16,6 +37,13 @@ void traverseTree(std::string directory){
     traverseTreeHelper(root, path);
 }
 
+/* traverseTreeHelper()
+* Purpose: Helps traverseTree to print each root-leaf path in the FSTree.
+           This is a recursive function.
+* Input: A pointer to a dirNode, and a reference to a vector containing the 
+         names of every file in the paths.
+* Return: Nothing
+*/
 void traverseTreeHelper(DirNode *node, std::vector<string>& path){
     if (node->isEmpty()){
         return;
@@ -46,7 +74,12 @@ void traverseTreeHelper(DirNode *node, std::vector<string>& path){
     path.pop_back();
 }
 
-
+/* main()
+* Purpose: Main function for FSTreeTraversal.cpp
+* Input: An int representing the number of command line arguments, and an 
+         array containing these arguments.
+* Return: Nothing
+*/
 int main(int argc, char *argv[]){
     if(argc == 2){
         traverseTree(argv[1]);
