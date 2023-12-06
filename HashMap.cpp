8275a5a3b26af5a1f7075d6f91f9a1e6ff246102
cmccal04 <152649028+cmccal04@users.noreply.github.com>
2023-12-06 11:42:05 -0500

    Add files via upload

---
+++ HashMap.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <functional>
+#include "HashMap.h"
+
+template <typename K, typename V>
+HashMap<K, V>::HashMap(){
+
+}
+
+// Insert key-value pair into the map
+template <typename K, typename V>
+void HashMap<K, V>::insert(const K& key, const V& value) {
+
+}
+
+// Get value associated with a given key
+template <typename K, typename V>
+V HashMap<K, V>::get(const K& key) const {
+    
+}
